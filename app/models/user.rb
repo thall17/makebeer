6d5b9133b4f4fa0e5f847017b507adf63d23a199
Tim Hall <thall74@gatech.edu>
2017-07-31 22:25:17 -0700

    Change menu to say username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :recipes
-  # validates :username, :email, presence: true, uniqueness: true
+  validates :username, :email, presence: true, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
