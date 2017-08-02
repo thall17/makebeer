1ad62a536607bcbf3329f9fc4b2a3bdd7d6cd022
Tim Hall <thall74@gatech.edu>
2017-08-02 09:02:50 -0700

    Add example model spec test

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "orders by name" do
+    user1 = User.create!(username: "user1", password: "password", email: "user1@user1.com")
+    recipe1 = Recipe.create!(name: "recipe1", user: user1)
+    recipe2 = Recipe.create!(name: "recipe2", user: user1)
+
+    expect(Recipe.order('name asc')).to eq([recipe1, recipe2])
+  end
 end
