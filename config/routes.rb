0b62331022dc17e81c9f033c5063faed4e7bdf18
Tim Hall <thall74@gatech.edu>
2017-12-22 13:39:34 -0800

    Add favicon

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  match '*path', to: redirect('/503.html'), via: [:get, :post]
   resources :recipes
-  devise_for :users, :controllers => { registrations: 'registrations' }  
+  devise_for :users, :controllers => { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'recipes#index'
 end
